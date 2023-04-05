4383d1b94852813f060721d98f1c6eff4f4c83ef
mposiul <mposiul@gmail.com>
2023-04-05 01:22:13 -0500

    cloudinary upload image ok

--- veterinaria/gestion/serializers.py
+++ veterinaria/gestion/serializers.py
@@ -1,7 +1,18 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Usuario
+from .models import Usuario, Mascota
 
 class RegistroUsuarioSerializer(ModelSerializer):
     class Meta:
         model = Usuario
         fields = '__all__'
+
+class MascotasSerializer(ModelSerializer):
+    class Meta:
+        model = Mascota
+        fields = '__all__'
+
+    def to_representation(self, instance):
+        print(instance.foto.url)
+        representacion = super().to_representation(instance)
+        representacion['foto'] = instance.foto.url
+        return representacion
--- veterinaria/gestion/views.py
+++ veterinaria/gestion/views.py
@@ -1,9 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.request import Request
-from .serializers import RegistroUsuarioSerializer
-from .models import Usuario
+from .serializers import RegistroUsuarioSerializer, MascotasSerializer
+from .models import Usuario, Mascota
 from rest_framework.response import Response
 from rest_framework import status
+# https://www.django-rest-framework.org/api-guide/permissions/
+from rest_framework.permissions import IsAuthenticated
+from .permisions import SoloClientes
+from cloudinary import uploader
 
 class RegistroUsuario(APIView):
     def post(self, request: Request):
@@ -22,3 +26,62 @@ class RegistroUsuario(APIView):
                 'message': 'Error al crear el usuario',
                 'content': serializador.errors
             }, status=status.HTTP_400_BAD_REQUEST)
+
+class PerfilUsuario(APIView):
+    # lo hara en orden > > > primero entrara al permiso de IsAuthenticate y luego si esta todo bien pasara al segundo permiso y asi sucesivamente, si algun permiso falla absolutamente se detiene todos los demas permisos
+    permission_classes = [IsAuthenticated, SoloClientes]
+
+    def get(self, request: Request):
+        print(request.user)
+        print(request.auth)
+        # TODO: Devolver el usuario, NO DEVOLVER LA PASSWORD solamente el nombre, apellido, correo y tipoUsuario utilizando un serializador
+        return Response(data={
+            'content': ''
+        })
+    
+class MascotasView(APIView):
+    #serializer_class = MascotasSerializer
+    permission_classes = [IsAuthenticated, SoloClientes]
+
+    def post(self, request:Request):
+        foto = request.FILES.get('foto')
+        print(request.data.get('nombre'))
+        
+        #resultado = uploader.upload(foto)
+        try:
+            data = {
+                'nombre': request.data.get('nombre'),
+                'sexo': request.data.get('sexo'),
+                'fechaNacimiento': request.data.get('fechaNacimiento'),
+                'alergias': request.data.get('alergias'),
+                'foto': request.FILES.get('foto'),
+                'cliente': request.data.get('cliente')
+            }
+
+            serializador = MascotasSerializer(data=data)
+            if serializador.is_valid():
+                serializador.save()
+                return Response(data= {
+                    'message': 'Mascota creada exitosamente',
+                    'content': serializador.data
+                }, status=status.HTTP_201_CREATED)
+            else:
+                return Response(data={
+                    'message': 'Error al crear mascota',
+                    'content': serializador.errors
+                }, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response(data={
+                'message': 'Error al crear mascota',
+                'content': str(e)
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
+    def get(self, request:Request):
+        mascotas = Mascota.objects.all()
+        serializador = MascotasSerializer(mascotas, many=True)
+
+        return Response(data={
+            'content': serializador.data
+        }, status=status.HTTP_200_OK)
+
+
